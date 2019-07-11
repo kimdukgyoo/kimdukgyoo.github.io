1f9b13bd29974230d7c55520e362bab980243314
Duk Gyoo Kim <kim.dukgyoo@gmail.com>
2019-07-11 13:25:40 -0400

    Update gcite_kim.R

--- Misc/gcite_kim.R
+++ Misc/gcite_kim.R
@@ -11,3 +11,6 @@ pdf("gcite_title_cloud.pdf")
 titles = paper_df$title
 title_cloud(titles)
 dev.off()
+
+### google scholar에서 abstract를 다운받은 다음 거기에 나온 words를 가지고 word cloud를 만드는 코드
+### collaborators가 많은 경우 collaborator cloud도 예쁘게 나옴
